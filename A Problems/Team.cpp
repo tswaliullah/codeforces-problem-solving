7ce61a5fefc4a57340fe4a2f547ad3aa396ea555
MD Waliullah <waliullah.godev@gmail.com>
2025-12-11 16:27:19 +0600

    team

--- A Problems/Team.cpp
+++ A Problems/Team.cpp
@@ -3,7 +3,28 @@ using namespace std;
 
 int main()
 {
+    int t;
+    cin >> t;
+
+    int cnt = 0;
+    while (t--)
+    {
+        int a[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            cin >> a[i];
+        }
+
+        int total = a[0] + a[1] + a[2];
+
+        if (total >= 2)
+        {
+            cnt++;
+        }
+    }
     
-    
+    cout << cnt << endl;
+
     return 0;
 }
